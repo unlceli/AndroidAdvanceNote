4c54b1d5b9bdd365b50401d429082422212c3da8
lizhe998 <125956902@qq.com>
2019-01-29 23:38:19 +0800

    activity

--- app/src/main/java/advance/li/androidadvancenote/accidence/View03/MyFristViewActivity.java
+++ app/src/main/java/advance/li/androidadvancenote/accidence/View03/MyFristViewActivity.java
@@ -3,6 +3,7 @@ package advance.li.androidadvancenote.accidence.View03;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import advance.li.androidadvancenote.R;
 
@@ -11,5 +12,12 @@ public class MyFristViewActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_frist);
+
+        findViewById(R.id.btn_standard).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
